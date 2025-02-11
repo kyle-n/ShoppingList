cc4c5d5b009a8723cd7d4bafc0e7bec545404fa9
Kyle Nazario <knazario@apple.com>
2025-02-11 11:02:29 -0700

    Implements basic shopping list app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState<Array<{name: string; id: string}>>([])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <header>
+      <h1>Shopping List</h1>
+    </header>
+    <main>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          const form = event.target as HTMLFormElement
+          const input = form.elements.namedItem('item') as HTMLInputElement
+          setItems([...items, { name: input.value, id: Date.now().toString() }])
+          input.value = ''
+        }}
+        style={{display: 'flex'}}
+      >
+        <input type="text" id="item" placeholder="Item name" style={{marginRight: '1rem'}} />
+        <button type="submit">Add</button>
+      </form>
+      <hr style={{margin: '2rem 0'}} />
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </main>
     </>
   )
 }
