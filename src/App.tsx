30941a5adaf0b8e6b4aac5efacdfd398e7bdf31b
Kyle Nazario <knazario@apple.com>
2025-02-11 11:14:59 -0700

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import ItemInput from './ItemInput';
 import { Item } from './types';
 
 function App() {
-  const [items, setItems] = useState<Item[]>([])
+  const [items, setItems] = useState<Item[]>([]);
 
   return (
     <>
-    <header>
-      <h1>Shopping List</h1>
-    </header>
-    <main>
-      <ItemInput onSubmit={newItem => setItems([...items, newItem])} />
-      <hr style={{margin: '2rem 0'}} />
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-    </main>
+      <header>
+        <h1>Shopping List</h1>
+      </header>
+      <main>
+        <ItemInput onSubmit={newItem => setItems([...items, newItem])} />
+        <hr style={{ margin: '2rem 0' }} />
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/ItemInput.tsx
+++ src/ItemInput.tsx
@@ -1,25 +1,30 @@
-import { Item } from './types'
+import { Item } from './types';
 
 type Props = {
-  onSubmit: (newItem: Item) => void
-}
+  onSubmit: (newItem: Item) => void;
+};
 
-function ItemInput({onSubmit}: Props) {
+function ItemInput({ onSubmit }: Props) {
   return (
-      <form
-        onSubmit={(event) => {
-          event.preventDefault()
-          const form = event.target as HTMLFormElement
-          const input = form.elements.namedItem('item') as HTMLInputElement
-          onSubmit({ name: input.value, id: Date.now().toString() })
-          input.value = ''
-        }}
-        style={{display: 'flex'}}
-      >
-        <input type="text" id="item" placeholder="Item name" style={{marginRight: '1rem'}} />
-        <button type="submit">Add</button>
-      </form>
-  )
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        const form = event.target as HTMLFormElement;
+        const input = form.elements.namedItem('item') as HTMLInputElement;
+        onSubmit({ name: input.value, id: Date.now().toString() });
+        input.value = '';
+      }}
+      style={{ display: 'flex' }}
+    >
+      <input
+        type="text"
+        id="item"
+        placeholder="Item name"
+        style={{ marginRight: '1rem' }}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
 }
 
-export default ItemInput
+export default ItemInput;
